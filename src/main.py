62e5ace78a4652c8ceb8da63b2355fb392933a5b
ygilm <ygilm@bk.ru>
2023-05-17 18:55:56 +0400

    added files

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from src.utils import read_operations, print_last_n_operations
+
+if __name__ == '__main__':
+    data = read_operations()
+    print_last_n_operations(data)
---
+++ tests/test_utils.py
@@ -0,0 +1,137 @@
+import os.path
+import pytest
+from src.utils import read_operations, last_n_operations, format_operation, print_last_n_operations
+
+PATH_DATA = os.path.abspath('./data')
+OPERATIONS_FILE_PATH = os.path.join(PATH_DATA, 'operations.json')
+
+def test_read_operations():
+    assert read_operations(OPERATIONS_FILE_PATH) is not None
+    with pytest.raises(FileNotFoundError):
+        read_operations('fake/path/to/operations.json')
+
+
+def test_last_n_operations():
+    data = [
+        {
+            "id": 716496732,
+            "state": "EXECUTED",
+            "date": "2018-04-04T17:33:34.701093",
+            "operationAmount": {
+                "amount": "40701.91",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Gold 5999414228426353",
+            "to": "Счет 72731966109147704472"
+        },
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+            "operationAmount": {
+                "amount": "41096.24",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 90424923579946435907"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }]
+
+    data_sorted = [
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+            "operationAmount": {
+                "amount": "41096.24",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 90424923579946435907"
+        },
+        {
+            "id": 716496732,
+            "state": "EXECUTED",
+            "date": "2018-04-04T17:33:34.701093",
+            "operationAmount": {
+                "amount": "40701.91",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Gold 5999414228426353",
+            "to": "Счет 72731966109147704472"
+        }
+    ]
+
+    assert type(last_n_operations(data, n=2, state='EXECUTED')) is list
+    assert last_n_operations(data, n=2, state='EXECUTED') == data_sorted
+
+
+def test_format_operation():
+    operations = [
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+            "operationAmount": {
+                "amount": "41096.24",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 90424923579946435907"
+        },
+        {
+            "id": 716496732,
+            "state": "EXECUTED",
+            "date": "2018-04-04T17:33:34.701093",
+            "operationAmount": {
+                "amount": "40701.91",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Gold 5999414228426353",
+            "to": "Счет 72731966109147704472"
+        }
+    ]
+
+    expected_outputs = [
+        "08.12.2019 Открытие вклада:\nNone -> Счет **5907\n41096.24 USD",
+        "04.04.2018 Перевод организации:\nVisa Gold 5999 14** **** 6353 -> Счет **4472\n40701.91 USD"
+    ]
+
+    for i, operation in enumerate(operations):
+        result = format_operation(operation)
+        assert result.strip() == expected_outputs[i].strip()
